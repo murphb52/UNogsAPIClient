f3048f87f067303c3abd34278f28cdd715b4bf28
Brian <brianmartinmurphy@gmail.com>
2020-02-07 16:14:01 +0000

    Adding ability to fetch new titles

--- Sources/UNogsAPI/UNogsAPIClient.swift
+++ Sources/UNogsAPI/UNogsAPIClient.swift
@@ -24,4 +24,23 @@ public class UNogsAPIClient {
         return publisher
     }
 
+    public func fetchNewReleases() -> AnyPublisher<ItemsResponse<NetflixTitle>, Error>  {
+        let headers = [
+            "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+            "x-rapidapi-key": "78baf00b28mshbf956afb933554bp178222jsn8a38b0e16183"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AGB&p=1&t=ns&st=adv")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let publisher = URLSession.shared.dataTaskPublisher(for: request as URLRequest)
+            .map { $0.data }
+            .decode(type: ItemsResponse<NetflixTitle>.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        return publisher
+    }
+
 }
