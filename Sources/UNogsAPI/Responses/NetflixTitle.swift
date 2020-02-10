b38d653b063cef2ad23724071f33700a4834bc31
Brian <brianmartinmurphy@gmail.com>
2020-02-10 08:41:37 +0000

    Adding requests and tests for new releases, expiring and filtered titles

--- Sources/UNogsAPI/Responses/NetflixTitle.swift
+++ Sources/UNogsAPI/Responses/NetflixTitle.swift
@@ -12,6 +12,22 @@ public struct NetflixTitle: Codable, Equatable, Identifiable {
     public enum TitleType : String, Codable {
         case series
         case movie
+
+        public init(from decoder: Decoder) throws {
+            // The API is unreliable and have seen instances of Movies and movie being returned so we must massage the data
+            let container = try decoder.singleValueContainer()
+            let string = try decoder.singleValueContainer().decode(String.self)
+            let lowerCasedString = string.lowercased()
+            guard let type = TitleType(rawValue: lowerCasedString) else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unknown TitleType: \(string)")
+            }
+            self = type
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            try container.encode(self.rawValue)
+        }
     }
 
     public var id: String { return netflixid }
--- Tests/UNogsAPITests/UNogsAPITests.swift
+++ Tests/UNogsAPITests/UNogsAPITests.swift
@@ -13,44 +13,62 @@ final class UNogsAPITests: XCTestCase {
     }
 
     func testCountries() {
-        let testExpectation = expectation(description: #function)
-
-        sut.fetchCountries()
-            .sink(receiveCompletion: { (completion) in
-                XCTAssertNil(completion.error)
-                testExpectation.fulfill()
-            },
-                  receiveValue: { (response) in
-                    XCTAssertEqual(response.count, "34")
-                    XCTAssertEqual(response.objects.count, 34)
-            })
-            .store(in: &disposabes)
-
-        waitForExpectations(timeout: 5, handler: nil)
+        assert(publisher: sut.countriesPublisher()) { response in
+            response.objects.forEach { print($0) }
+            XCTAssertEqual(response.count, "34")
+            XCTAssertEqual(response.objects.count, 34)
+        }
     }
 
     func testNewReleases() {
-        let testExpectation = expectation(description: #function)
+        assert(publisher: sut.newReleasesPublisher()) { response in
+            response.objects.forEach { print($0) }
+            XCTAssertEqual(response.count, "30")
+            XCTAssertEqual(response.objects.count, 30)
+        }
+    }
 
-        sut.fetchNewReleases()
-            .sink(receiveCompletion: { (completion) in
-                XCTAssertNil(completion.error)
-                testExpectation.fulfill()
-            },
-                  receiveValue: { (response) in
-                    response.objects.forEach { print($0) }
-                    XCTAssertEqual(response.count, "89")
-                    XCTAssertEqual(response.objects.count, 89)
-            })
-            .store(in: &disposabes)
+    func testExpiring() {
+        assert(publisher: sut.expiringPublisher()) { response in
+            response.objects.forEach { print($0) }
+            XCTAssertEqual(response.count, "66")
+            XCTAssertEqual(response.objects.count, 66)
+        }
+    }
 
-        waitForExpectations(timeout: 5, handler: nil)
+    func testFilteredTitles() {
+        assert(publisher: sut.filteredTitlesPublisher()) { response in
+            response.objects.forEach { print($0) }
+            XCTAssertEqual(response.count, "11106")
+            XCTAssertEqual(response.objects.count, 100)
+        }
     }
 
     static var allTests = [
         ("testCountries", testCountries),
         ("testNewReleases", testNewReleases),
     ]
+}
+
+private extension UNogsAPITests {
+
+    func assert<T>(publisher: AnyPublisher<ItemsResponse<T>, Error>,
+                   receiveClosure: @escaping (_ response: ItemsResponse<T>) -> Void) {
+        let testExpectation = expectation(description: #function)
+
+        publisher
+            .sink(receiveCompletion: { (completion) in
+                XCTAssertNil(completion.error)
+                testExpectation.fulfill()
+            },
+                  receiveValue: { (response) in
+                    receiveClosure(response)
+            })
+            .store(in: &disposabes)
+
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
 }
 
 extension Subscribers.Completion {
