74bd087470c5e2824d93a770eb87e65d5408563a
Brian <brianmartinmurphy@gmail.com>
2020-02-18 20:42:39 +0000

    Getting CodeCoverage for current version up to 100%

--- Sources/UNogsAPI/Responses/NetflixTitle.swift
+++ Sources/UNogsAPI/Responses/NetflixTitle.swift
@@ -40,41 +40,4 @@ public struct NetflixTitle: Codable, Equatable, Identifiable {
     public let released: String
     public let runtime: String
     public let unogsdate: String
-    /*
-     "title":"Heidi"
-     "image":"http://art-1.nflximg.net/de202/fd3380ee585b86954bd7ed91dd331f1d02dde202.jpg"
-     "synopsis":"Orphaned at age 5, curly-haired Heidi is sent to live with her gruff recluse of a grandfather in the Swiss Alps and soon thaws his frozen heart.<br><b>New on 2017-08-05</b>"
-     "rating":"0"
-     "type":"movie"
-     "released":"2015"
-     "runtime":"1h50m"
-     "largeimage":""
-     "unogsdate":"2017-08-05"
-     "imdbid":"tt3700392"
-     "download":"0"
-     */
-
-    public init(netflixid: String,
-                title: String,
-                image: String,
-                synopsis: String,
-                rating: String,
-                type: TitleType,
-                released: String,
-                runtime: String,
-                unogsdate: String) {
-        self.netflixid = netflixid
-        self.title = title
-        self.image = image
-        self.synopsis = synopsis
-        self.rating = rating
-        self.type = type
-        self.released = released
-        self.runtime = runtime
-        self.unogsdate = unogsdate
-    }
-}
-
-extension NetflixTitle: CustomDebugStringConvertible {
-    public var debugDescription: String { "\(type), \(unogsdate), \(rating): \(title)" }
 }
